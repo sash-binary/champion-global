c9ada0a59baeafdec77593f9d4c11e4f818482f2
ashkan <ashkan.nemati@gmail.com>
2019-11-21 14:50:51 +0100

    Updates

---
+++ component---src-pages-404-js-cc185a9a9c75d0143832.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{w2l6:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),u=n("8k0H"),o=n("Y9I6");t.default=function(){return l.a.createElement(u.a,null,l.a.createElement(o.a,{title:"404: Not found"}),l.a.createElement("h1",null,"NOT FOUND"),l.a.createElement("p",null,"You just hit a route that doesn't exist."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-cc185a9a9c75d0143832.js.map
